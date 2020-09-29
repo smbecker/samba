c28764e0eb33082278bed1cfa767ef5edec2b20e
Jeremy Allison <jra@samba.org>
2020-09-29 13:08:01 -0700

    s3: smbd: SMB1 rename_internals() can figure out the wildcard status of the paths by itself.
    No need to pass them as parameters.
    
    Signed-off-by: Jeremy Allison <jra@samba.org>
    Reviewed-by: Ralph Boehme <slow@samba.org>

--- source3/smbd/reply.c
+++ source3/smbd/reply.c
@@ -7966,8 +7966,6 @@ NTSTATUS rename_internals(TALLOC_CTX *ctx,
 			const char *dst_original_lcomp,
 			uint32_t attrs,
 			bool replace_if_exists,
-			bool src_has_wild,
-			bool dest_has_wild,
 			uint32_t access_mask)
 {
 	char *fname_src_dir = NULL;
@@ -7983,6 +7981,8 @@ NTSTATUS rename_internals(TALLOC_CTX *ctx,
 	bool posix_pathnames = (req != NULL && req->posix_pathnames);
 	struct smb2_create_blobs *posx = NULL;
 	int rc;
+	bool src_has_wild = false;
+	bool dest_has_wild = false;
 
 	/*
 	 * Split the old name into directory and last component
@@ -7999,6 +7999,18 @@ NTSTATUS rename_internals(TALLOC_CTX *ctx,
 	if (!NT_STATUS_IS_OK(status)) {
 		status = NT_STATUS_NO_MEMORY;
 		goto out;
+	}
+
+	if (req != NULL && !req->posix_pathnames) {
+		/*
+		 * Check the wildcard mask *before*
+		 * unmangling. As mangling is done
+		 * for names that can't be returned
+		 * to Windows the unmangled name may
+		 * contain Windows wildcard characters.
+		 */
+		src_has_wild = ms_has_wild(fname_src_mask);
+		dest_has_wild = ms_has_wild(dst_original_lcomp);
 	}
 
 	/*
@@ -8473,8 +8485,6 @@ void reply_mv(struct smb_request *req)
 				dst_original_lcomp,
 				attrs,
 				false,
-				src_has_wcard,
-				dest_has_wcard,
 				DELETE_ACCESS);
 	if (!NT_STATUS_IS_OK(status)) {
 		if (open_was_deferred(req->xconn, req->mid)) {
