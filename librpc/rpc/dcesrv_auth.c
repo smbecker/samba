151435042dbf77ce89fecb35712bda32c699206e
Volker Lendecke <vl@samba.org>
2021-02-02 15:07:35 +0100

    librpc: Make "dcesrv_context->callbacks" a pointer
    This structure just grew from 3 to 6 pointers, avoid making a copy of
    this. All callers of dcesrv_init_context() have this as a static
    struct in the C object, so a pointer to that won't change.
    
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- librpc/rpc/dcesrv_auth.c
+++ librpc/rpc/dcesrv_auth.c
@@ -81,7 +81,7 @@ static bool dcesrv_auth_prepare_gensec(struct dcesrv_call_state *call)
 {
 	struct dcesrv_connection *dce_conn = call->conn;
 	struct dcesrv_auth *auth = call->auth_state;
-	struct dcesrv_context_callbacks *cb = &call->conn->dce_ctx->callbacks;
+	struct dcesrv_context_callbacks *cb = call->conn->dce_ctx->callbacks;
 	NTSTATUS status;
 
 	if (auth->auth_started) {
@@ -251,7 +251,7 @@ void dcesrv_default_auth_state_prepare_request(struct dcesrv_call_state *call)
 {
 	struct dcesrv_connection *dce_conn = call->conn;
 	struct dcesrv_auth *auth = call->auth_state;
-	struct dcesrv_context_callbacks *cb = &call->conn->dce_ctx->callbacks;
+	struct dcesrv_context_callbacks *cb = call->conn->dce_ctx->callbacks;
 
 	if (auth->auth_audited) {
 		return;
@@ -289,7 +289,7 @@ bool dcesrv_auth_bind(struct dcesrv_call_state *call)
 {
 	struct ncacn_packet *pkt = &call->pkt;
 	struct dcesrv_auth *auth = call->auth_state;
-	struct dcesrv_context_callbacks *cb = &call->conn->dce_ctx->callbacks;
+	struct dcesrv_context_callbacks *cb = call->conn->dce_ctx->callbacks;
 	NTSTATUS status;
 
 	if (pkt->auth_length == 0) {
