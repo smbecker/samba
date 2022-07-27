390ca074311718db618db1c9f886472e5b8c0edf
Ralph Boehme <slow@samba.org>
2022-07-27 16:04:24 +0200

    smdb: use fsp_is_alternate_stream() in open_file()
    No change in behaviour.
    
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=15126
    MR: https://gitlab.com/samba-team/samba/-/merge_requests/2643
    
    Signed-off-by: Ralph Boehme <slow@samba.org>
    Reviewed-by: Volker Lendecke <vl@samba.org>
    (cherry picked from commit 0d3995cec10c5fae8c8b6a1df312062e38437e6f)

--- source3/smbd/open.c
+++ source3/smbd/open.c
@@ -1421,7 +1421,7 @@ static NTSTATUS open_file(struct smb_request *req,
 #endif
 
 		/* Don't create files with Microsoft wildcard characters. */
-		if (fsp->base_fsp) {
+		if (fsp_is_alternate_stream(fsp)) {
 			/*
 			 * wildcard characters are allowed in stream names
 			 * only test the basefilename
@@ -1437,7 +1437,7 @@ static NTSTATUS open_file(struct smb_request *req,
 		}
 
 		/* Can we access this file ? */
-		if (!fsp->base_fsp) {
+		if (!fsp_is_alternate_stream(fsp)) {
 			/* Only do this check on non-stream open. */
 			if (file_existed) {
 				status = smbd_check_access_rights_fsp(
